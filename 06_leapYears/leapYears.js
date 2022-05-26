d6d376576d783fec72ac614e7c277a41a34bfe0b
CJ Eslinger <cjeslinger28@gmail.com>
2022-05-26 12:07:31 -0400

    finished leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,25 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const centuryYear = checkCenturyYear(year);
+    if (!centuryYear) {
+        return checkIfDivBy4(year);
+    } else return centuryIsLeap(year);
 };
 
+function checkCenturyYear(num) {
+    if (num % 100 == 0) {
+        return true;
+    } else return false;
+}
+
+function checkIfDivBy4(num) {
+    if (num % 4 == 0) {
+        return true;
+    } else return false;
+}
+
+function centuryIsLeap (num) {
+    num /= 100
+    return checkIfDivBy4(num);
+}
 // Do not edit below this line
 module.exports = leapYears;
