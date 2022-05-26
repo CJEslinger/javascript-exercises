7f093fa11d9c75dc87c16472df9111d08b52fe93
CJ Eslinger <cjeslinger28@gmail.com>
2022-05-26 12:20:40 -0400

    finished num 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,18 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  const tempC = (tempF - 32) * (5/9);
+  return roundNum(tempC);
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  const tempF = (tempC * (9/5)) +32
+  return roundNum(tempF);
 };
+
+function roundNum (num) {
+  const roundedNum = Math.round(num * 10) / 10;
+  const fixed = roundedNum.toFixed(1);
+  return parseFloat(fixed);
+}
 
 // Do not edit below this line
 module.exports = {
