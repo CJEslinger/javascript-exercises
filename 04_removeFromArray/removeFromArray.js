e26024d68bcd5f26280a96cefc2977a6ae5aff53
CJ Eslinger <cjeslinger28@gmail.com>
2022-05-26 10:15:05 -0400

    finished 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
 
-};
+const removeFromArray = function(...arguements) {
+    const array = arguements[0]
+    const newArray = []
+    array.forEach(element => {
+        if (!arguements.includes(element)) {
+            newArray.push(element);
+        }
+    });
+    return newArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
