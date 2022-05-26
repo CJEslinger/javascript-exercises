94b33aed035f263d175358de7bc8db1f0291b9bb
CJ Eslinger <cjeslinger28@gmail.com>
2022-05-25 21:33:51 -0400

    3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+let arr;
+let reverseArr;
+const reverseString = function(string) {
+    arr = string.split("");
+    reverseArr = arr.reverse();
+    return reverseArr.join("");
 };
 
 // Do not edit below this line
