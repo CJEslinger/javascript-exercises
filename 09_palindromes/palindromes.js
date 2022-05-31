8f0fa59d059d5d3421a83b10265b8c8f6cf79fa8
CJ Eslinger <cjeslinger28@gmail.com>
2022-05-31 17:49:00 -0400

    finished some exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase()
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+    .replace(/\s/g, '');
 
+    const arr = str.split('');
+    arr.reverse();
+    const newStr = arr.join('');
+    console.log(`${newStr}\n${str}`);
+    return newStr == str ? true : false;
 };
 
 // Do not edit below this line
